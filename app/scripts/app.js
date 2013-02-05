0682cc9f68f20f848e632aa9232d7d843a062791
mranosa <mark.ranosa@gmail.com>
2013-02-06 03:51:54 +0800

    Hello yeoman + h4ckathon

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mustachedCyrilApp = angular.module('mustachedCyrilApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+mustachedCyrilApp.controller('MainCtrl', 
+	['$scope', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+}]);
